5366685dc9ee72951229be19356c7c1a8fb4f4cf
rumcho660 <85699721+rumcho660@users.noreply.github.com>
2023-01-07 17:02:39 +0200

    working window descriptor

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
 use bevy::window::{CompositeAlphaMode, CursorGrabMode, PresentMode};
 use bevy::prelude::*;
+use bevy::window::CursorIcon::Default;
 
 
 fn main() {
     App::new()
-        .insert_resource(WindowDescriptor{
-            title: "Dr. Covid".to_string(),
-            ..Default::default()
-        })
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(WindowPlugin{
+            window: WindowDescriptor{
+                title: "Dr. Covid".to_string(),
+                ..default()
+            },
+            ..default()
+        }))
         .run();
 
 }
