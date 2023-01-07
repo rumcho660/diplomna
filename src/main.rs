85e50539fd4b9a67cb3654e935f9cde4906be782
rumcho660 <85699721+rumcho660@users.noreply.github.com>
2023-01-07 16:51:55 +0200

    default plugins and window descriptor

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use bevy::window::{CompositeAlphaMode, CursorGrabMode, PresentMode};
+use bevy::prelude::*;
+
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .insert_resource(WindowDescriptor{
+            title: "Dr. Covid".to_string(),
+            ..Default::default()
+        })
+        .add_plugins(DefaultPlugins)
+        .run();
+
 }
+
