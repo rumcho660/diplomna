f20fedf19523c111f37f3f0c6d736c0a3cc36028
rumcho660 <85699721+rumcho660@users.noreply.github.com>
2023-01-25 22:20:27 +0200

    timer almost done

--- src/main.rs
+++ src/main.rs
@@ -32,72 +32,97 @@ struct TimerEndGame (Timer);
 
 
 fn timer_til_game_end(mut timer_end: ResMut<TimerEndGame>, mut _exit: EventWriter<AppExit>, mut commands: Commands, asset_server: Res<AssetServer>){
-    let font = asset_server.load("fonts/SuPostcode-VGeLe.ttf");
+
+    let font = asset_server.load("Yesillow.otf");
     let text_style = TextStyle {
         font,
-        font_size: 60.0,
-        color: Color::WHITE,
+        font_size: 100.0,
+        color: Color::RED,
     };
+
+
 
     timer_end.0.tick(Duration::from_secs_f32(0.01));
 
 
-    if timer_end.0.elapsed_secs() == 0.0 {
-        commands.spawn(Text2dBundle {
-            text: Text::from_section("0", text_style.clone()),
+    if timer_end.0.elapsed_secs() == 1.0{
+        let onesec = commands.spawn(Text2dBundle {
+            text: Text::from_section("1 sec", text_style.clone()),
             ..default()
         });
-    }
 
-
-    else if timer_end.0.elapsed_secs() == 1.0{
-        println!("1")
     }
 
     else if timer_end.0.elapsed_secs() == 2.0{
-        println!("2");
+        commands.spawn(Text2dBundle {
+            text: Text::from_section("1 sec 2 sec", text_style.clone()),
+            ..default()
+        });
 
     }
 
     else if timer_end.0.elapsed_secs() == 3.0{
-        println!("3");
-
+        commands.spawn(Text2dBundle {
+            text: Text::from_section("1 sec 2 sec 3 sec", text_style.clone()),
+            ..default()
+        });
 
     }
 
     else if timer_end.0.elapsed_secs() == 4.0{
-        println!("4");
+        commands.spawn(Text2dBundle {
+            text: Text::from_section("1 sec 2 sec 3 sec 4 sec", text_style.clone()),
+            ..default()
+        });
 
     }
 
     else if timer_end.0.elapsed_secs() == 5.0{
-        println!("5");
+        commands.spawn(Text2dBundle {
+            text: Text::from_section("1 sec 2 sec 3 sec 4 sec 5 sec", text_style.clone()),
+            ..default()
+        });
 
     }
 
 
     else if timer_end.0.elapsed_secs() == 6.0{
-        println!("6");
+        commands.spawn(Text2dBundle {
+            text: Text::from_section("1 sec 2 sec 3 sec 4 sec 5 sec 6 sec", text_style.clone()),
+            ..default()
+        });
 
     }
 
     else if timer_end.0.elapsed_secs() == 7.0{
-        println!("7");
+        commands.spawn(Text2dBundle {
+            text: Text::from_section("1 sec 2 sec 3 sec 4 sec 5 sec 6 sec 7 sec", text_style.clone()),
+            ..default()
+        });
 
     }
 
     else if timer_end.0.elapsed_secs() == 8.0{
-        println!("8");
+        commands.spawn(Text2dBundle {
+            text: Text::from_section("1 sec 2 sec 3 sec 4 sec 5 sec 6 sec 7 sec 8 sec", text_style.clone()),
+            ..default()
+        });
 
     }
 
     else if timer_end.0.elapsed_secs() == 9.0{
-        println!("9");
+        commands.spawn(Text2dBundle {
+            text: Text::from_section(" 1 sec 2 sec 3 sec 4 sec 5 sec 6 sec 7 sec 8 sec 9 sec", text_style.clone()),
+            ..default()
+        });
 
     }
 
     else if timer_end.0.elapsed_secs() == 10.0{
-        println!("10");
+        commands.spawn(Text2dBundle {
+            text: Text::from_section("1 sec 2 sec 3 sec 4 sec 5 sec 6 sec 7 sec 8 sec 9 sec 10 sec", text_style.clone()),
+            ..default()
+        });
         _exit.send(AppExit);
     }
 
