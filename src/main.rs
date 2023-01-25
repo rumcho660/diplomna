a21d09c8c1969012205f3bbcd7388234e416e452
rumcho660 <85699721+rumcho660@users.noreply.github.com>
2023-01-25 17:56:21 +0200

    simple working stopwatch

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
+use std::iter::Once;
+use std::time::Duration;
 use bevy:: prelude::*;
+use bevy::app::AppExit;
 
 
 fn main() {
     App::new()
+        .insert_resource(TimerOfGame(Timer::from_seconds(15.0, TimerMode::Once)))
         .add_plugins(DefaultPlugins.set(WindowPlugin{
             window: WindowDescriptor{
                 title: "Dr. Covid".to_string(),
@@ -11,9 +15,21 @@ fn main() {
             ..default()
         }))
         .add_startup_system(setup_camera)
+        .add_system(timer1)
         .run();
 }
 
 fn setup_camera(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
+}
+
+#[derive(Resource)]
+struct TimerOfGame (Timer);
+
+
+fn timer1(time: Res<Time>, mut timer: ResMut<TimerOfGame> ,mut exit: EventWriter<AppExit>){
+    if timer.0.tick(time.delta()).just_finished(){
+        println!("ohh");
+        exit.send(AppExit);
+    }
 }
